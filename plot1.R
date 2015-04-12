50618b24317d951739e04d8eb0b8266c7606d182
Luz Martínez <pcgirl4@hotmail.com>
2015-04-12 17:41:12 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,6 +2,8 @@ url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_co
 archivo <- "./exdata-data-household_power_consumption.zip"
 tabla <- "household_power_consumption.txt"
 
+## Se verifica si existe
+
 if(!file.exists(archivo)){
   download.file(url, destfile = archivo)}
 
@@ -12,6 +14,7 @@ if(!file.exists(tabla)){
   unzip("./exdata-data-household_power_consumption.zip")
 }
 
+## Se extraen datos
 datos <- read.table(tabla, head = TRUE, sep =";", 
                     colClasses = c("character", "character", rep("numeric",7)), na = "?")
 
@@ -21,7 +24,7 @@ datos$Date <- as.Date(datos$Date, "%d/%m/%Y")
 fechas <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
 datos <- subset(datos, Date %in% fechas)
 
-
+## se crea el plot
 png("plot1.png", width=400, height=400)
 hist(datos$Global_active_power, col="red",  xlab = "Global Active Power (kilowatts)", 
      ylab = "Frequency", main = "Global Active Power")
